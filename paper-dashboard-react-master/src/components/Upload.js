eebd80f165b407b70375722c02af01e0420e8a5c
anjalisoni3655 <anjalisony32@gmail.com>
2021-03-14 17:30:56 +0530

    show toast on upload

--- paper-dashboard-react-master/src/components/Upload.js
+++ paper-dashboard-react-master/src/components/Upload.js
@@ -1,58 +1,103 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-import { useFileUpload } from 'use-file-upload'
-import axios from "axios"
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Toast from "react-bootstrap/Toast";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+import axios from "axios";
 // import '../App.css';
 
-class Upload extends React.Component{
+class Upload extends React.Component {
   // const [file, selectFile] = useFileUpload()
   constructor(props) {
     super(props);
     this.state = {
-        imageURL: "",
+      imageURL: "",
+      isUploaded: false,
     };
     this.handleUploadImage = this.handleUploadImage.bind(this);
-}
+  }
 
-handleUploadImage(ev) {
+  handleUploadImage(ev) {
     ev.preventDefault();
 
     const data = new FormData();
-    data.append('file', this.uploadInput.files[0]);
+    data.append("file", this.uploadInput.files[0]);
     // data.append('filename', this.fileName.value);
 
-    fetch('http://localhost:5000/upload', { method: 'POST', body: data })
-    .then((response) => { response.json().then((body) => { 
-        alert("Your file is being uploaded!");      
-        this.setState({ imageURL: `http://localhost:5000/uploads/${body.file}` });
-      });
-    });
-  }
-
-render() {
-    return (
-      <div>       
-        <form onSubmit={this.handleUploadImage}>
-          <div>
-            <input ref={(ref) => { this.uploadInput = ref; }} type="file" />
-            <Button>Upload</Button>       
-          </div>
-        </form>
-        <br />
-          <div>
-            OR
-          </div>        
-          <br />          
-        <form onSubmit={this.handleUploadImage}>              
-          <div>
-          <input ref={(ref) => { this.uploadInput = ref; }} type="file" name="Choose from sample data"/>
-            <Button >Upload</Button>               
-          </div>
-        </form>
-      </div>  
+    fetch("http://localhost:5000/upload", { method: "POST", body: data }).then(
+      (response) => {
+        response.json().then((body) => {
+          alert("Your file is being uploaded!");
+          this.state.isUploaded = true;
+          console.log("isuploaded", this.state.isUploaded);
+          this.setState({
+            imageURL: `http://localhost:5000/uploads/${body.file}`,
+          });
+        });
+      }
     );
   }
 
+  render() {
+    return (
+      <div>
+        {this.state.isUploaded && (
+          <Row>
+            <Col xs={6}>
+              <Toast
+                onClose={() => (this.state.isUploaded = false)}
+                show={this.state.isUploaded}
+                delay={3000}
+                autohide
+              >
+                <Toast.Header>
+                  <img
+                    src="holder.js/20x20?text=%20"
+                    className="rounded mr-2"
+                    alt=""
+                  />
+                  <strong className="mr-auto">Bootstrap</strong>
+                  <small>11 mins ago</small>
+                </Toast.Header>
+                <Toast.Body>
+                  Woohoo, you're reading this text in a Toast!
+                </Toast.Body>
+              </Toast>
+            </Col>
+          </Row>
+        )}
+        <form onSubmit={this.handleUploadImage}>
+          <div>
+            <div style={{ color: "black" }}>Upload new Data Set</div>
+            <input
+              ref={(ref) => {
+                this.uploadInput = ref;
+              }}
+              type="file"
+            />
+            <Button>Upload</Button>
+          </div>
+        </form>
+        <br />
+        <div>OR</div>
+        <br />
+        <form onSubmit={this.handleUploadImage}>
+          <div>
+            <div style={{ color: "black" }}>Choose from sample data</div>
+            <input
+              ref={(ref) => {
+                this.uploadInput = ref;
+              }}
+              type="file"
+              name="Choose from sample data"
+            />
+            <Button>Upload</Button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Upload
+export default Upload;
