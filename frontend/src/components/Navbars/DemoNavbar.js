83aefb0c136fb4887579546e9ae2a34a49c13b13
Anjali Soni <anjalisony32@gmail.com>
2021-03-14 21:28:00 +0530

    update ui (#1)

---
+++ frontend/src/components/Navbars/DemoNavbar.js
@@ -0,0 +1,143 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Container,
+  InputGroup,
+  InputGroupText,
+  InputGroupAddon,
+  Input,
+} from "reactstrap";
+
+import routes from "routes.js";
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      dropdownOpen: false,
+      color: "transparent",
+    };
+    this.toggle = this.toggle.bind(this);
+    this.dropdownToggle = this.dropdownToggle.bind(this);
+    this.sidebarToggle = React.createRef();
+  }
+  toggle() {
+    if (this.state.isOpen) {
+      this.setState({
+        color: "transparent",
+      });
+    } else {
+      this.setState({
+        color: "dark",
+      });
+    }
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+  dropdownToggle(e) {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+    });
+  }
+  getBrand() {
+    let brandName = "Default Brand";
+    routes.map((prop, key) => {
+      if (window.location.href.indexOf(prop.layout + prop.path) !== -1) {
+        brandName = prop.name;
+      }
+      return null;
+    });
+    return brandName;
+  }
+  openSidebar() {
+    document.documentElement.classList.toggle("nav-open");
+    this.sidebarToggle.current.classList.toggle("toggled");
+  }
+  // function that adds color dark/transparent to the navbar on resize (this is for the collapse)
+  updateColor() {
+    if (window.innerWidth < 993 && this.state.isOpen) {
+      this.setState({
+        color: "dark",
+      });
+    } else {
+      this.setState({
+        color: "transparent",
+      });
+    }
+  }
+  componentDidMount() {
+    window.addEventListener("resize", this.updateColor.bind(this));
+  }
+  componentDidUpdate(e) {
+    if (
+      window.innerWidth < 993 &&
+      e.history.location.pathname !== e.location.pathname &&
+      document.documentElement.className.indexOf("nav-open") !== -1
+    ) {
+      document.documentElement.classList.toggle("nav-open");
+      this.sidebarToggle.current.classList.toggle("toggled");
+    }
+  }
+  render() {
+    return (
+      // add or remove classes depending if we are on full-screen-maps page or not
+      <Navbar
+        color={
+          this.props.location.pathname.indexOf("full-screen-maps") !== -1
+            ? "dark"
+            : this.state.color
+        }
+        expand="lg"
+        className={
+          this.props.location.pathname.indexOf("full-screen-maps") !== -1
+            ? "navbar-absolute fixed-top"
+            : "navbar-absolute fixed-top " +
+              (this.state.color === "transparent" ? "navbar-transparent " : "")
+        }
+      >
+        <Container fluid>
+          <div className="navbar-wrapper">
+            <div className="navbar-toggle">
+              <button
+                type="button"
+                ref={this.sidebarToggle}
+                className="navbar-toggler"
+                onClick={() => this.openSidebar()}
+              >
+                <span className="navbar-toggler-bar bar1" />
+                <span className="navbar-toggler-bar bar2" />
+                <span className="navbar-toggler-bar bar3" />
+              </button>
+            </div>
+            <NavbarBrand href="/">{this.getBrand()}</NavbarBrand>
+          </div>
+          <NavbarToggler onClick={this.toggle}>
+            <span className="navbar-toggler-bar navbar-kebab" />
+            <span className="navbar-toggler-bar navbar-kebab" />
+            <span className="navbar-toggler-bar navbar-kebab" />
+          </NavbarToggler>
+          <Collapse
+            isOpen={this.state.isOpen}
+            navbar
+            className="justify-content-end"
+          >
+          </Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default Header;
---
+++ frontend/src/routes.js
@@ -0,0 +1,69 @@
+import Dashboard from "views/Dashboard.js";
+import Notifications from "views/Notifications.js";
+import Icons from "views/Icons.js";
+import Typography from "views/Typography.js";
+import TableList from "views/Tables.js";
+import Maps from "views/Map.js";
+import UserPage from "views/User.js";
+import UpgradeToPro from "views/Upgrade.js";
+
+var routes = [
+  // {
+  //   path: "/dashboard",
+  //   name: "Dashboard",
+  //   icon: "nc-icon nc-bank",
+  //   component: Dashboard,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/icons",
+  //   name: "Icons",
+  //   icon: "nc-icon nc-diamond",
+  //   component: Icons,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/maps",
+  //   name: "Maps",
+  //   icon: "nc-icon nc-pin-3",
+  //   component: Maps,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/notifications",
+  //   name: "Notifications",
+  //   icon: "nc-icon nc-bell-55",
+  //   component: Notifications,
+  //   layout: "/admin",
+  // },
+  {
+    path: "/modify-dataset",
+    name: "Modify Dataset",
+    icon: "nc-icon nc-single-02",
+    component: UserPage,
+    layout: "/admin",
+  },
+  {
+    path: "/view-data-stats",
+    name: "View Data Stats",
+    icon: "nc-icon nc-tile-56",
+    component: TableList,
+    layout: "/admin",
+  },
+  {
+    path: "/view-model-stat",
+    name: "View Model Stats",
+    icon: "nc-icon nc-caps-small",
+    component: Typography,
+    layout: "/admin",
+  },
+  // {
+  //   pro: true,
+  //   path: "/upgrade",
+  //   name: "Upgrade to PRO",
+  //   icon: "nc-icon nc-spaceship",
+  //   component: UpgradeToPro,
+  //   layout: "/admin",
+  // },
+];
+export default routes;
