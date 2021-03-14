96516aaaddc2a177f92e18e56f98a042156fc85b
Tushar Bajaj <tbajaj@iitg.ac.in>
2021-03-14 14:33:23 +0530

    added ui

---
+++ paper-dashboard-react-master/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/paper-dashboard.scss?v=1.2.0";
+import "assets/demo/demo.css";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+
+import AdminLayout from "layouts/Admin.js";
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+      <Redirect to="/admin/dashboard" />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
---
+++ paper-dashboard-react-master/src/routes.js
@@ -0,0 +1,69 @@
+import Dashboard from "views/Dashboard.js";
+import Notifications from "views/Notifications.js";
+import Icons from "views/Icons.js";
+import Typography from "views/Typography.js";
+import TableList from "views/Tables.js";
+import Maps from "views/Map.js";
+import UserPage from "views/User.js";
+import UpgradeToPro from "views/Upgrade.js";
+
+var routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "nc-icon nc-bank",
+    component: Dashboard,
+    layout: "/admin",
+  },
+  {
+    path: "/icons",
+    name: "Icons",
+    icon: "nc-icon nc-diamond",
+    component: Icons,
+    layout: "/admin",
+  },
+  {
+    path: "/maps",
+    name: "Maps",
+    icon: "nc-icon nc-pin-3",
+    component: Maps,
+    layout: "/admin",
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: "nc-icon nc-bell-55",
+    component: Notifications,
+    layout: "/admin",
+  },
+  {
+    path: "/user-page",
+    name: "User Profile",
+    icon: "nc-icon nc-single-02",
+    component: UserPage,
+    layout: "/admin",
+  },
+  {
+    path: "/tables",
+    name: "Table List",
+    icon: "nc-icon nc-tile-56",
+    component: TableList,
+    layout: "/admin",
+  },
+  {
+    path: "/typography",
+    name: "Typography",
+    icon: "nc-icon nc-caps-small",
+    component: Typography,
+    layout: "/admin",
+  },
+  {
+    pro: true,
+    path: "/upgrade",
+    name: "Upgrade to PRO",
+    icon: "nc-icon nc-spaceship",
+    component: UpgradeToPro,
+    layout: "/admin",
+  },
+];
+export default routes;
