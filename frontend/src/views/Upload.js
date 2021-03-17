4feea5523d38aae1d67a9c56b52af4379410b925
kousikr26 <kousik18@iitg.ac.in>
2021-03-17 18:18:00 +0530

    New toast UI

--- frontend/src/views/Upload.js
+++ frontend/src/views/Upload.js
@@ -5,115 +5,46 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
 import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
 
+import 'react-toastify/dist/ReactToastify.css';
 class Upload extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      imageURL: "",
-      isUploaded: false,
-      isError: false,
-      errorMessage: "",
-    };
+    
     this.handleUploadImage = this.handleUploadImage.bind(this);
-    this.fileValidate = this.fileValidate.bind(this);
+    
   }
 
-  /*  var axios = require('axios');
-
-   */
-  fileValidate() {
-    var fileInput = document.getElementById("file");
-
-    var filePath = fileInput.value;
-
-    // Allowing file type
-    var allowedExtensions = ".zip";
-    console.log("Checking extension")
-    if (!allowedExtensions.exec(filePath)) {
-      alert("Invalid file type");
-      fileInput.value = "";
-      return false;
-    }
-  }
-
+  
+ 
   async handleUploadImage(ev) {
     ev.preventDefault();
 
     const data = new FormData();
     data.append("file", this.uploadInput.files[0]);
-    // data.append('filename', this.fileName.value);
+    
     
     const res = await axios.post("http://localhost:5000/upload", data);
 
     
 
     if (res.data == "OK") {
-      this.setState({
-        isUploaded: true,
-      });
+      
+      toast.success('🦄 Data uploaded succesfully');
       
     } else {
-        this.setState({
-          isError: true,
-          errorMessage: res.data,
-        });
-        
-        
+      toast.error("💀 Error : "+res.data);
     }
 
-    console.log("isuploade", this.state.isUploaded);
-    console.log("iserror", this.state.isError);
-    console.log(res);
-    console.log(res.data);
   }
-  handleClose() {
-    this.setState({
-      isUploaded: false,
-      isError: false,
-      errorMessage:"",
-    });
-  }
+ 
 
   render() {
     return (
       <div>
-        {this.state.isUploaded && (
-          <Toast
-            onClose={this.handleClose.bind(this)}
-            show={this.state.isUploaded}
-            delay={3000}
-            autohide
-            style={{
-              position: "absolute",
-              top: -90,
-              right: -20,
-              background:"green",
-            }}
-          >
-            <Toast.Header>
-              <strong className="mr-auto">File Uploaded Successfully</strong>
-            </Toast.Header>
-          </Toast>
-        )}
-        {this.state.isError && (
-          <Toast
-            onClose={this.handleClose.bind(this)}
-            show={this.state.isError}
-            delay={3000}
-            autohide
-            style={{
-              position: "absolute",
-              top: -90,
-              right: -20,
-              background: "red",
-            }}
-          >
-            <Toast.Header>
-              <strong className="mr-auto">File Upload error, {this.state.errorMessage}</strong>
-            </Toast.Header>
-          </Toast>
-        )}
+        
+        
         <form>
           {/* <div style={{ color: "black" }}>Add New Data</div> */}
           <div style={{ textAlign: "center" }}>
@@ -130,7 +61,7 @@ class Upload extends React.Component {
 
               <Col>
                 <Button
-                  onChange={this.fileValidate}
+                  
                   onClick={this.handleUploadImage}
                 >
                   Upload
@@ -139,6 +70,7 @@ class Upload extends React.Component {
             </Row>
           </div>
         </form>
+        <ToastContainer />
       </div>
     );
   }
